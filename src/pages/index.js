d2a3fd09831a217ae0e276a84a8a4b4dcd9aef98
Faryln <Mail.Faryln@gmail.com>
2018-06-26 15:25:03 +0200

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import styled from "styled-components"
 
 const contentWrapper = styled("div")({
   display: "flex",
