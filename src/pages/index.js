6c54ae10631cb745fcde99bef3e69b4932d4a8bb
Faryln <Mail.Faryln@gmail.com>
2018-06-26 16:31:14 +0200

    Start Reading

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default class IndexPage extends React.Component {
                   <br />
                   <br />
                   <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
+                    Start Reading →
                   </Link>
                 </p>
               </div>
